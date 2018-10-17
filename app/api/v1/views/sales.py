91b1b973c9e06fc3b5ac64b5fa9ad9cb501afaca
Scott Francis <scotfrank28@gmail.com>
2018-10-17 09:40:41 +0300

    [#161269245]getting all sales api

--- app/api/v1/views/sales.py
+++ app/api/v1/views/sales.py
@@ -1,18 +1,18 @@
-# from flask import Flask, make_response, jsonify, request
-# from flask_restful import Resource, Api, reqparse
+from flask import Flask, make_response, jsonify, request
+from flask_restful import Resource, Api, reqparse
 
-# #from app.api.models import products
+#from app.api.models import products
 
-# sales = []
+sales = []
 
-# class Sales(Resource):
+class Sales(Resource):
 	
-# 	def get(self):
-# 		"""Endpoint for fetching all products"""
-# 		return jsonify(sales)
-# 		return jsonify({'message':'Item not found'},
-# 						{'status': 200}
-# 			)
+	def get(self):
+		"""Endpoint for fetching all products"""
+		return jsonify(sales)
+		return jsonify({'message':'Item not found'},
+						{'status': 200}
+			)
 
 
 
